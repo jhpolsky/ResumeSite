71cf6da7af6b77fae7c527234e6affa946b1991a
Joseph Polsky <jhpolsky@gmail.com>
2026-04-04 23:37:49 -0400

    typos in counter

--- counter.js
+++ counter.js
@@ -1,3 +1,3 @@
-const visitors = fetch("mycounturl")
+const visitors = fetch("https://ej6k6nb4fe.execute-api.us-east-1.amazonaws.com/count")
 .then(response => response.json())
-.then(count => { document.getElementById("counter").textContent = count.count; });
+.then(count => { document.getElementById("counter").textContent = count; });
